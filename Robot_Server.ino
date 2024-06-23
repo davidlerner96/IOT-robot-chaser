18969081d23d6202d8046799ecae836f122923f1
maorheruti <86756960+maorheruti@users.noreply.github.com>
2024-06-23 04:40:36 +0300

    Update Robot_Server.ino
    edited command to set angle of steering

--- Robot_Server.ino
+++ Robot_Server.ino
@@ -20,7 +20,11 @@ web server that lets you send commands to the Robot via the web.
  */
 #include <L298NX2.h>
 #include <WiFi.h>
+#include <ESP32Servo.h>
 
+
+Servo myServo;
+const unsigned int servoPin=18;
 // Motors Pin definition
 const unsigned int IN1 = 27;
 const unsigned int IN2 = 26;
@@ -43,6 +47,7 @@ NetworkServer server(80);
 void setup() {
   delay(1000);
   Serial.begin(115200);
+  myServo.attach(servoPin);
   delay(2000);
   pinMode(2, OUTPUT);  // set the LED pin mode
 
@@ -74,6 +79,7 @@ void loop() {
     bool isForward_2 = 0;
     int speed_1 = 0;
     int speed_2 = 0;
+    bool is_angle = 0;
 int reconnect_count=1;
       while (WiFi.status() != WL_CONNECTED) {
         motors.stop(); 
@@ -108,11 +114,11 @@ int reconnect_count=1;
             // the content of the HTTP response follows the header:
             client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
             client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
-            client.print("Click <a href=\"/F254F254\">here</a> to drive forward fast.<br>");
-            client.print("Click <a href=\"/F150F150\">here</a> to drive forward slow.<br>");
-            client.print("Click <a href=\"/B254B254\">here</a> to drive backward fast.<br>");
-            client.print("Click <a href=\"/B150B150\">here</a> to drive backward slow.<br>");
-            client.print("Click <a href=\"/F000F000\">here</a> to stop.<br>");             
+            client.print("Click <a href=\"/F254F254A120A\">here</a> to drive forward fast.<br>");
+            client.print("Click <a href=\"/F150F150A120A\">here</a> to drive forward slow.<br>");
+            client.print("Click <a href=\"/B254B254A120A\">here</a> to drive backward fast.<br>");
+            client.print("Click <a href=\"/B150B150A120A\">here</a> to drive backward slow.<br>");
+            client.print("Click <a href=\"/F000F000A120A\">here</a> to stop.<br>");             
             // The HTTP response ends with another blank line:
             client.println();
             // break out of the while loop:
@@ -131,17 +137,27 @@ int reconnect_count=1;
         if (currentLine.endsWith("GET /L")) {
           digitalWrite(2, LOW);  // GET /L turns the LED off
         }
-        if(currentLine.length()==13&&currentLine.startsWith("GET")){
-//           Serial.println();
-//                      Serial.print("str is: ");
-//            Serial.println(currentLine);
-//            Serial.print("str len is: ");
-//  Serial.println(currentLine.length(),DEC);
+        if(currentLine.length()==18&&currentLine.startsWith("GET")){
+  //          Serial.println();
+  //                     Serial.print("str is: ");
+  //           Serial.println(currentLine);
+  //           Serial.print("str len is: ");
+  // Serial.println(currentLine.length(),DEC);
 
           char M1_Direction = currentLine[5];
           String M1_Speed = currentLine.substring(6,9);
           char M2_Direction = currentLine[9];
-          String M2_Speed = currentLine.substring(10);
+          String M2_Speed = currentLine.substring(10,13);
+          String angle_wrap = currentLine.substring(13);
+          String angle_str =  currentLine.substring(14,17);
+          if(angle_wrap=="NNNNN")
+            is_angle = 0;
+          else if(angle_wrap[0]=='A' && angle_wrap[4]=='A')
+            is_angle = 1;
+          else
+            continue;
+          //   Serial.print("angle_wrap is "+ angle_wrap+ ", is_angle: ");
+          //  Serial.println(is_angle);
           if(M1_Direction == 'F')
             isForward_1 = 1;
            else if(M1_Direction == 'B')
@@ -157,6 +173,12 @@ int reconnect_count=1;
 
           int speed_1 = M1_Speed.toInt();
           int speed_2 = M2_Speed.toInt();
+          int angle = angle_str.toInt();
+          if(is_angle){
+            Serial.print("angle set to: ");
+            Serial.println(angle);
+            myServo.write(angle);
+          }
                                Serial.print("speed_1 set to: ");
            Serial.println(speed_1);
                                           Serial.print(", isForward_1: ");
@@ -165,6 +187,8 @@ int reconnect_count=1;
            Serial.println(speed_2);
                                           Serial.print(", isForward_2: ");
            Serial.println(isForward_2);
+           
+
           motors.setSpeedA(speed_1);
           motors.setSpeedB(speed_2);
           if(isForward_1)
